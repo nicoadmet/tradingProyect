e3b1f14a9984b9156f3057b904a322972c93cdd2
nicoadmet <admetnico@gmail.com>
2024-08-12 22:44:48 -0300

    commit 4
    agregue el cambio de color de las celdas dependiendo de si el ROI y la ganancia es positivo o negativo

--- javascript/calculos.js
+++ javascript/calculos.js
@@ -2,7 +2,6 @@
 
 // funcion que calcula el ROI
 function calcularRoi(fila) {
-
     try {
         
         //parseFloat convierte el numero a Float, querySelector obtiene el valor de (""), si no tiene valor es igual a 0 
@@ -10,6 +9,9 @@ function calcularRoi(fila) {
         var precioEntrada = parseFloat(fila.querySelector(".precio-entrada").value) || 0;
         var precioSalida = parseFloat(fila.querySelector(".precio-salida").value) || 0;
         var inversion = parseFloat(fila.querySelector(".inversion").value) || 0;
+        
+        var celdaShortLong = fila.querySelector(".celda-compra-venta");
+        var celdaLong = fila.querySelector(".long");
 
         var roi;
         var gananciaPerdida
@@ -20,20 +22,55 @@ function calcularRoi(fila) {
         } else if (tipoOperacion === "SHORT") {
             roi = (((precioEntrada - precioSalida) / inversion) * 10).toFixed(2);
             gananciaPerdida = ((precioEntrada - precioSalida) * (inversion / precioEntrada)).toFixed(2);
+            
+            //color celda de compra/venta
+            celdaShortLong.style.backgroundColor = "rgb(0, 153, 102)";
+
         } else { // LONG
             roi = (((precioSalida - precioEntrada) / inversion) * 10).toFixed(2);
             gananciaPerdida = ((precioSalida - precioEntrada) * (inversion / precioEntrada)).toFixed(2);
+
+            //color celda de compra/venta
+            celdaShortLong.style.backgroundColor = "rgb(204, 51, 51)";
         }
         
         //le da los valores a las cosas que estamos calculando
         fila.querySelector(".roi").value = roi + '%';
         fila.querySelector(".ganancia-perdida").value = gananciaPerdida;
 
+
+        /////color de las celdas/////
+        var inputRoi = fila.querySelector(".roi");
+        var celdaRoi = fila.querySelector(".celda-roi");
+
+        var inputGanancias = fila.querySelector(".celda-ganancia-perdida");
+        var celdaGanancias = fila.querySelector(".ganancia-perdida");
+
+        if (roi > 0) {
+            celdaRoi.style.backgroundColor = "rgb(0, 153, 102)";
+            inputRoi.style.backgroundColor = "rgb(1, 179, 120)";
+
+            celdaGanancias.style.backgroundColor = "rgb(0, 153, 102)";
+            inputGanancias.style.backgroundColor = "rgb(1, 179, 120)";
+            
+        } else if (roi < 0) { 
+            celdaRoi.style.backgroundColor = "rgb(204, 51, 51)";
+            inputRoi.style.backgroundColor = "rgb(201, 64, 64)";
+
+            celdaGanancias.style.backgroundColor = "rgb(204, 51, 51)";
+            inputGanancias.style.backgroundColor = "rgb(201, 64, 64)";
+        }
+
     } catch (e) { // e = error
         console.error('Error en el calculo del ROI:', e);
     }
     
 }
+
+
+
+
+
 
 
 
